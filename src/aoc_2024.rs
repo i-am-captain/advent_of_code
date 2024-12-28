91e96f555f8d8e4bd3a63d51a641202e1dfd3d20
i-am-captain <i-am-captain@users.noreply.github.com>
2024-12-28 02:05:09 +0100

    Day 3

--- src/aoc_2024.rs
+++ src/aoc_2024.rs
@@ -1,7 +1,9 @@
 mod aoc_2024_day_01;
 mod aoc_2024_day_02;
+mod aoc_2024_day_03;
 
 pub(crate) fn run_all() {
     aoc_2024_day_01::run();
     aoc_2024_day_02::run();
+    aoc_2024_day_03::run();
 }
